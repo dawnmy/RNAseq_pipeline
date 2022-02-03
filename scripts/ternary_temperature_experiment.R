f68af9e267e9c38326a090afa15702144f2f4930
Z.-L. Deng <dawnmsg@gmail.com>
2022-02-03 11:30:57 +0100

    Create ternary_temperature_experiment.R

---
+++ scripts/ternary_temperature_experiment.R
@@ -0,0 +1,15 @@
+library(ggtern)
+
+## Run this script only after run the DE script
+pm_rel_logcpm5 <- pm_rel[y$AveLogCPM >5,]
+
+
+ggtern(data = data.frame(st20 = rowMeans(pm_rel_logcpm5[,4:7]),
+                                 st26 = rowMeans(pm_rel_logcpm5[,10:12]),
+                                 st30 = rowMeans(pm_rel_logcpm5[,16:18]),
+                                 logCPM = y$AveLogCPM[y$AveLogCPM >1]),
+               aes(st20, st26, st30, size=logCPM)) + 
+  geom_point(alpha=0.4) +
+  theme_rgbw(base_size = 15) 
+  labs(title = "Expression distribution among 20, 26 and 30 at stationary phase")    +
+  guides(color = "none", fill = "none", alpha = "none")
